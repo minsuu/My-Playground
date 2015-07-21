95b45033504af1d776c170adcf93ab90a54b1089
minsuu <ps_ir@naver.com>
2015-07-21 17:00:41 +0900

    Initial Commit

---
+++ csss/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  csss
+//
+//  Created by minsu on 2015. 7. 21..
+//  Copyright (c) 2015년 minsu. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
